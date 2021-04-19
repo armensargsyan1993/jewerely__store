af80b2fe97f39f229d127d8fdf92c0fbe136590b
armensargsyan1993 <50384664+armensargsyan1993@users.noreply.github.com>
2021-04-19 21:38:47 +0400

    add assets

--- src/components/assets.ts
+++ src/components/assets.ts
@@ -1,17 +1,94 @@
+import arrowRight from '../assets/icons/Polygon 2.svg'
+import visa from '../assets/icons/Rectangle 13.svg'
+import masterCard from '../assets/icons/Rectangle 14.svg'
+import unknownUser from '../assets/icons/Vector-1.svg'
+import search from '../assets/icons/Vector-2.svg'
+import diamond from '../assets/icons/Vector-3.svg'
+import phone from '../assets/icons/Vector-4.svg'
+import message from '../assets/icons/Vector-5.svg'
+import vk from '../assets/icons/Vector-6.svg'
+import facebook from '../assets/icons/Vector-7.svg'
+import paperAirplane from '../assets/icons/Vector-8.svg'
+import explorer from '../assets/icons/Vector-9.svg'
+import heart from '../assets/icons/Vector.svg'
 
+import homeBg from '../assets/pictures/bgc.png'
+import gallery_1 from '../assets/pictures/gallery__image.png'
+import gallery_2 from '../assets/pictures/gallery__image-1.png'
+import gallery_3 from '../assets/pictures/gallery__image-2.png'
+import gallery_4 from '../assets/pictures/gallery__image-3.png'
+import rect5_1 from '../assets/pictures/Rectangle 5.png'
+import rect5_2 from '../assets/pictures/Rectangle 5-1.png'
+import rect5_3 from '../assets/pictures/Rectangle 5-2.png'
+import rect5_4 from '../assets/pictures/Rectangle 5-3.png'
+import rect5_5 from '../assets/pictures/Rectangle 5-4.png'
+import rect5_6 from '../assets/pictures/Rectangle 5-5.png'
+import rect5_7 from '../assets/pictures/Rectangle 5-6.png'
+import rect5_8 from '../assets/pictures/Rectangle 5-7.png'
+import rect5_9 from '../assets/pictures/Rectangle 5-8.png'
 
 export const icons:IIcons = {
-
+    arrowRight,
+    visa,
+    masterCard,
+    unknownUser,
+    search,
+    diamond,
+    phone,
+    message,
+    vk,
+    facebook,
+    paperAirplane,
+    explorer,
+    heart,
 }
 
 export const pictures:IPictures = {
-
+    homeBg,
+    gallery_1,
+    gallery_2,
+    gallery_3,
+    gallery_4,
+    rect5_1,
+    rect5_2,
+    rect5_3,
+    rect5_4,
+    rect5_5,
+    rect5_6,
+    rect5_7,
+    rect5_8,
+    rect5_9,
 }
 
 interface IIcons {
-
+    arrowRight:string
+    visa:string
+    masterCard:string
+    unknownUser:string
+    search:string
+    diamond:string
+    phone:string
+    message:string
+    vk:string
+    facebook:string
+    paperAirplane:string
+    explorer:string
+    heart:string
 }
 
 interface IPictures {
-
+    homeBg:string
+    gallery_1:string
+    gallery_2:string
+    gallery_3:string
+    gallery_4:string
+    rect5_1:string
+    rect5_2:string
+    rect5_3:string
+    rect5_4:string
+    rect5_5:string
+    rect5_6:string
+    rect5_7:string
+    rect5_8:string
+    rect5_9:string
 }
