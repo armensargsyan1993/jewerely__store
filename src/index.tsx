91aa0f088503574eb4993614da48097256d62e0c
armensargsyan1993 <50384664+armensargsyan1993@users.noreply.github.com>
2021-04-19 15:45:07 +0400

    bs config....

--- src/index.tsx
+++ src/index.tsx
@@ -2,26 +2,12 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 import { Provider } from 'react-redux'
 import { BrowserRouter } from 'react-router-dom'
-import { Action, applyMiddleware, createStore } from 'redux'
-import logger from 'redux-logger'
-//add localStorage
-import { persistReducer, persistStore } from 'redux-persist'
+import { Action } from 'redux'
 import { PersistGate } from 'redux-persist/integration/react'
-import storage from 'redux-persist/lib/storage'
-import thunk, { ThunkAction } from 'redux-thunk'
+import { ThunkAction } from 'redux-thunk'
 import { App } from './App'
 import './index.scss'
-import { rootReducer } from './redux/rootReducer'
-
-const persistConfig = {
-  key: 'root',
-  storage,
-}
-
-const persistedReducer = persistReducer(persistConfig, rootReducer)
-
-let store = createStore(persistedReducer, applyMiddleware(thunk, logger))
-let persistor = persistStore(store)
+import { persistor, store } from './redux/store'
 
 ReactDOM.render(
   <React.StrictMode>
@@ -38,16 +24,17 @@ ReactDOM.render(
   document.getElementById('root')
 )
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-// reportWebVitals();
 
 export type RootState = ReturnType<typeof store.getState>
 
 export type ThunkType<T extends Action> = ThunkAction<
-  Promise<void> | void,
-  RootState,
-  unknown,
-  T
+Promise<void> | void,
+RootState,
+unknown,
+T
 >
+
+// If you want to start measuring performance in your app, pass a function
+// to log results (for example: reportWebVitals(console.log))
+// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+// reportWebVitals();
