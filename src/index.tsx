a93ebbc77d05593982ace3a61f86411538e82dd1
armensargsyan1993 <50384664+armensargsyan1993@users.noreply.github.com>
2021-04-19 15:42:02 +0400

    base config ...

--- src/index.tsx
+++ src/index.tsx
@@ -1,17 +1,17 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import { Provider } from 'react-redux';
-import { BrowserRouter } from 'react-router-dom';
-import { Action, applyMiddleware, createStore } from 'redux';
-import logger from 'redux-logger';
+import React from 'react'
+import ReactDOM from 'react-dom'
+import { Provider } from 'react-redux'
+import { BrowserRouter } from 'react-router-dom'
+import { Action, applyMiddleware, createStore } from 'redux'
+import logger from 'redux-logger'
 //add localStorage
-import { persistReducer, persistStore } from 'redux-persist';
-import { PersistGate } from 'redux-persist/integration/react';
-import storage from 'redux-persist/lib/storage';
-import thunk, { ThunkAction } from 'redux-thunk';
-import {App} from './App';
-import './index.scss';
-import { rootReducer } from './redux/rootReducer';
+import { persistReducer, persistStore } from 'redux-persist'
+import { PersistGate } from 'redux-persist/integration/react'
+import storage from 'redux-persist/lib/storage'
+import thunk, { ThunkAction } from 'redux-thunk'
+import { App } from './App'
+import './index.scss'
+import { rootReducer } from './redux/rootReducer'
 
 const persistConfig = {
   key: 'root',
@@ -20,37 +20,34 @@ const persistConfig = {
 
 const persistedReducer = persistReducer(persistConfig, rootReducer)
 
-let store = createStore(persistedReducer,applyMiddleware(
-  thunk,
-  logger,
-))
+let store = createStore(persistedReducer, applyMiddleware(thunk, logger))
 let persistor = persistStore(store)
 
 ReactDOM.render(
   <React.StrictMode>
     <Provider store={store}>
-    <PersistGate loading={null} persistor={persistor}>
-      <BrowserRouter
-      // basename="/index.html/"
-      >
-        <App />
-      </BrowserRouter>
+      <PersistGate loading={null} persistor={persistor}>
+        <BrowserRouter
+        // basename="/index.html/"
+        >
+          <App />
+        </BrowserRouter>
       </PersistGate>
     </Provider>
   </React.StrictMode>,
   document.getElementById('root')
-);
+)
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 // reportWebVitals();
 
-
 export type RootState = ReturnType<typeof store.getState>
 
 export type ThunkType<T extends Action> = ThunkAction<
-Promise<void> | void,
-RootState,
-unknown,
-T>
+  Promise<void> | void,
+  RootState,
+  unknown,
+  T
+>
