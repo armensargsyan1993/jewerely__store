7dc33fefe99cd329a107a0cb27adcb19d9c47bb0
armensargsyan1993 <50384664+armensargsyan1993@users.noreply.github.com>
2021-04-20 20:13:19 +0400

    add basi styles

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,9 @@
-import React, { useState } from "react";
+import React from 'react'
 
-export const App = () => {  
-
+export const App = () => {
   return (
-    <div></div>
+    <>
+    
+    </>
   )
 }
