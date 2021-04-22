8165dff67bd10342367a9d9716961d404ce149b2
armensargsyan1993 <50384664+armensargsyan1993@users.noreply.github.com>
2021-04-22 16:40:28 +0400

    add InputCreator

--- src/components/Search/Search.tsx
+++ src/components/Search/Search.tsx
@@ -1,28 +1,24 @@
-
 import React from 'react'
 import styles from './Search.module.scss'
-import { Input, InputAdornment } from '@material-ui/core'
 import { useDispatch } from 'react-redux'
 import { searchThunk } from '../../redux/search/searchActions'
 import { SvgCreator } from '../SvgCreator/SvgCreator'
 import { icons } from '../assets'
 
 export const Search = () => {
-    const dispatch = useDispatch()
-    const handleChange = (e:React.ChangeEvent<HTMLInputElement>) => {
-        dispatch(searchThunk(e.target.value))
-    } 
-    return (
-        <div className={styles.root}>
-            <Input 
-            type="search" 
-            onChange={handleChange}
-            startAdornment={
-                <InputAdornment position="start">
-                    <SvgCreator 
-                    clsName={styles.icon} svgHref={`${icons.search}#search`}/>
-                </InputAdornment>
-              }/>
-        </div>
-    )
+  const dispatch = useDispatch()
+  const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    dispatch(searchThunk(e.target.value))
+  }
+  const handleFocus = (e:React.ChangeEvent<HTMLInputElement>) => {
+      console.log(`object`);
+  }
+  return (
+    <div className={styles.root}>
+      <label>
+            <SvgCreator clsName={styles.icon} svgHref={`${icons.search}#search`} />
+          <input onFocus={handleFocus} type="search"/>
+      </label>
+    </div>
+  )
 }
