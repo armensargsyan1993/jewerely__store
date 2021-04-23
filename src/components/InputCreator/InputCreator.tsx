a54347ced2b06bc95763d6e625ee41437cbe12c7
armensargsyan1993 <50384664+armensargsyan1993@users.noreply.github.com>
2021-04-23 13:08:38 +0400

    delete r/bootstrap bootstrap

--- src/components/InputCreator/InputCreator.tsx
+++ src/components/InputCreator/InputCreator.tsx
@@ -1,11 +1,20 @@
 
-import React from 'react'
+import React, { useState } from 'react'
 import styles from './InputCreator.module.scss'
 
-export const InputCreator = () => {
+interface IInputProps {
+
+} 
+export const InputCreator:React.FC<IInputProps> = (props) => {
+    const [focus, setFocus] = useState(false)
+
+    const handleFocus = () => {
+        setFocus(() => true)
+    }
+
     return (
         <div className={styles.root}>
-            
+            <input onFocus={handleFocus} type="search"/>
         </div>
     )
 }
--- src/components/Search/Search.tsx
+++ src/components/Search/Search.tsx
@@ -4,37 +4,38 @@ import { useDispatch } from 'react-redux'
 import { searchThunk } from '../../redux/search/searchActions'
 import { SvgCreator } from '../SvgCreator/SvgCreator'
 import { icons } from '../assets'
-import { Button } from 'react-bootstrap';
 
 export const Search = () => {
   const dispatch = useDispatch()
   const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
     dispatch(searchThunk(e.target.value))
   }
-  const handleFocus = (e:React.ChangeEvent<HTMLInputElement>) => {
-      console.log(`object`);
+  const handleFocus = (e: React.ChangeEvent<HTMLInputElement>) => {
+    console.log(`object`)
   }
   return (
     <div className={styles.root}>
       <label>
-        <Button> asfsdf</Button>
-        {/* <Input
-          disableUnderline={true}
-          className={styles.input}
-          classes={{ focused: styles.focused }}
-          onChange={handleChange}
-          startAdornment={
-            <InputAdornment position="start">
-              <SvgCreator
-                clsName={styles.icon}
-                svgHref={`${icons.search}#search`}
-              />
-            </InputAdornment>
-          }
-        /> */}
-            <SvgCreator clsName={styles.icon} svgHref={`${icons.search}#search`} />
-          <input onFocus={handleFocus} type="search"/>
+        <SvgCreator clsName={styles.icon} svgHref={`${icons.search}#search`} />
+        <input onFocus={handleFocus} type="search"/>
       </label>
     </div>
   )
 }
+
+        // <Input
+        //   disableUnderline={true}
+        //   className={styles.input}
+        //   classes={{ focused: styles.focused }}
+        //   onChange={handleChange}
+        //   startAdornment={
+        //     <InputAdornment position="start">
+        //       <SvgCreator
+        //         clsName={styles.icon}
+        //         svgHref={`${icons.search}#search`}
+        //       />
+        //     </InputAdornment>
+        //   }
+        // /> 
+        // <SvgCreator clsName={styles.icon} svgHref={`${icons.search}#search`} />
+        // <input onFocus={handleFocus} type="search"/> 
