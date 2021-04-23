a8f9395174a3df9f05aadd1f5d2f177e5fc5afd2
armensargsyan1993 <50384664+armensargsyan1993@users.noreply.github.com>
2021-04-23 12:29:56 +0400

    delete MUI

--- src/components/Search/Search.tsx
+++ src/components/Search/Search.tsx
@@ -8,20 +8,16 @@ import { icons } from '../assets'
 export const Search = () => {
   const dispatch = useDispatch()
   const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
-<<<<<<< HEAD
-    // dispatch(searchThunk(e.target.value))
-=======
     dispatch(searchThunk(e.target.value))
   }
   const handleFocus = (e:React.ChangeEvent<HTMLInputElement>) => {
       console.log(`object`);
->>>>>>> 94f29bba9f1e4b1064405c57d99f48422ddeb4e0
   }
   return (
     <div className={styles.root}>
       <label>
-<<<<<<< HEAD
-        <Input
+        
+        {/* <Input
           disableUnderline={true}
           className={styles.input}
           classes={{ focused: styles.focused }}
@@ -34,11 +30,9 @@ export const Search = () => {
               />
             </InputAdornment>
           }
-        />
-=======
+        /> */}
             <SvgCreator clsName={styles.icon} svgHref={`${icons.search}#search`} />
           <input onFocus={handleFocus} type="search"/>
->>>>>>> 94f29bba9f1e4b1064405c57d99f48422ddeb4e0
       </label>
     </div>
   )
