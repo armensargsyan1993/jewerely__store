02ea228cf658027e2656f53212929346b0b0d440
armensargsyan1993 <50384664+armensargsyan1993@users.noreply.github.com>
2021-04-23 17:01:53 +0400

    complete navbar

--- src/components/InputCreator/InputCreator.tsx
+++ src/components/InputCreator/InputCreator.tsx
@@ -1,20 +1,52 @@
-
 import React, { useState } from 'react'
+import { icons } from '../assets'
+import { SvgCreator } from '../SvgCreator/SvgCreator'
 import styles from './InputCreator.module.scss'
 
 interface IInputProps {
+  onFocus?: () => void
+  onCHange?: (e: React.ChangeEvent<HTMLInputElement>) => void
+  right?:boolean
+}
+export const InputCreator: React.FC<IInputProps> = (props) => {
+  const [focus, setFocus] = useState(false)
+  const [value,setValue] = useState('')
 
-} 
-export const InputCreator:React.FC<IInputProps> = (props) => {
-    const [focus, setFocus] = useState(false)
+  const onFocus = () => {
+    setFocus(() => true)
+    props.onFocus && props.onFocus()
+  }
+  const onBlur = () => {
+    setFocus(() => false)
+  }
 
-    const handleFocus = () => {
-        setFocus(() => true)
-    }
+  const onChange = (e:React.ChangeEvent<HTMLInputElement>) => {
+    props.onCHange && props.onCHange(e)
+    setValue(() => e.target.value)
+  }
 
-    return (
-        <div className={styles.root}>
-            <input onFocus={handleFocus} type="search"/>
-        </div>
-    )
+  const jsxArr = [
+    <SvgCreator key={111} clsName={`${styles.icon} ${focus || value ? styles.iconFocus : ''}`} svgHref={`${icons.search}#search`} />,
+    <input
+        onChange={onChange}
+        className={`${styles.input} ${focus || value ? styles.inputFocus : ''}`}
+        onBlur={onBlur}
+        onFocus={onFocus}
+        value={value}
+        placeholder="search"
+        key={222}
+    />
+  ]
+
+  if(props.right){
+      jsxArr.reverse()
+  }
+
+  return (
+    <div className={styles.root}>
+        <label>
+            {jsxArr}
+        </label>
+    </div>
+  )
 }
--- src/components/Search/Search.tsx
+++ src/components/Search/Search.tsx
@@ -2,40 +2,18 @@ import React from 'react'
 import styles from './Search.module.scss'
 import { useDispatch } from 'react-redux'
 import { searchThunk } from '../../redux/search/searchActions'
-import { SvgCreator } from '../SvgCreator/SvgCreator'
-import { icons } from '../assets'
+import { InputCreator } from '../InputCreator/InputCreator'
 
 export const Search = () => {
   const dispatch = useDispatch()
-  const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
-    dispatch(searchThunk(e.target.value))
-  }
-  const handleFocus = (e: React.ChangeEvent<HTMLInputElement>) => {
-    console.log(`object`)
+  const onChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    // dispatch(searchThunk(e.target.value))
+    // console.log(e.target.value);
   }
   return (
     <div className={styles.root}>
-      <label>
-        <SvgCreator clsName={styles.icon} svgHref={`${icons.search}#search`} />
-        <input onFocus={handleFocus} type="search"/>
-      </label>
+        <InputCreator onCHange={onChange}/>
     </div>
+
   )
 }
-
-        // <Input
-        //   disableUnderline={true}
-        //   className={styles.input}
-        //   classes={{ focused: styles.focused }}
-        //   onChange={handleChange}
-        //   startAdornment={
-        //     <InputAdornment position="start">
-        //       <SvgCreator
-        //         clsName={styles.icon}
-        //         svgHref={`${icons.search}#search`}
-        //       />
-        //     </InputAdornment>
-        //   }
-        // /> 
-        // <SvgCreator clsName={styles.icon} svgHref={`${icons.search}#search`} />
-        // <input onFocus={handleFocus} type="search"/> 
