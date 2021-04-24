db3c476b0973ee44c6da6cbb8985689357361995
armensargsyan1993 <50384664+armensargsyan1993@users.noreply.github.com>
2021-04-24 12:06:05 +0400

    add Title

---
+++ src/components/Title/Title.tsx
@@ -0,0 +1,11 @@
+
+import React from 'react'
+import styles from './Title.module.scss'
+
+export const Title = () => {
+    return (
+        <div className={styles.root}>
+            
+        </div>
+    )
+}
