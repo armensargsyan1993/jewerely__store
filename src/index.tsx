faf5149b0b50a5e605aa41d7d91d00b03cdd7232
armensargsyan1993 <50384664+armensargsyan1993@users.noreply.github.com>
2021-05-02 13:14:39 +0400

    test

--- src/index.tsx
+++ src/index.tsx
@@ -36,6 +36,8 @@ unknown,
 T
 >
 
+//TEST
+
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
